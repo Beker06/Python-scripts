f08dcd61f4a2785b3b267735528f181d76409a1b
Eliam Vazquez <bosc790@gmail.com>
2023-11-03 20:50:40 -0700

    Random Password Generator

---
+++ Random Pass Generator/main.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+print("Password: ")
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+chars = lower+upper+num+symbols
+
+temp = random.sample(chars, 25)
+print("".join(temp))
